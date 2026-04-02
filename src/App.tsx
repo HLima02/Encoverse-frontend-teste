703d9b4f125b240f4caf93b928b995486f41c451
Hiago <hiago@redrex.tech>
2026-04-02 14:22:58 -0300

    feature: initial BrowserRouter config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useEffect } from 'react'
 import { useProdutoStore } from './store/useProdutoStore'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './router'
 
 export default function App() {
   const { produtos, loading, error, fetchProdutos } = useProdutoStore()
@@ -12,15 +14,6 @@ export default function App() {
   if (error) return <div>{error}</div>
 
   return (
-    <div>
-      <h1>Produtos ({produtos.length})</h1>
-      <ul>
-        {produtos.map((produto) => (
-          <li key={produto.productName}>
-            <strong>{produto.productName}</strong> - R$ {produto.price / 100}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <RouterProvider router={router} />
   )
 }
