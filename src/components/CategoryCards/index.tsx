ee9fff17599f6d62da5aa15a79b1f9e60445d5af
Hiago <hiago@redrex.tech>
2026-04-03 14:40:45 -0300

    Faature: alteração na cor da imagem dos cards em categorias ao settar como active utilizando filter css

--- src/components/CategoryCards/index.tsx
+++ src/components/CategoryCards/index.tsx
@@ -8,9 +8,15 @@ type CategoryCardsProps = {
 	image: string
 }
 
-export default function CategoryCards({ data }:{data:CategoryCardsProps}) {
+interface Props {
+  data: CategoryCardsProps
+  active?: boolean
+  onClick?: () => void
+}
+
+export default function CategoryCards({ data, active, onClick }: Props) {
   return (
-    <Link to={data.href} className='category_card'>
+    <Link to={data.href} className={`category_card ${active ? 'active' : ''}`} onClick={onClick}>
 		<div className='flex_center category_card_img'>
 			<img src={data.image} alt={data.label} />
 		</div>
--- src/sections/CategoryCardsSection/index.tsx
+++ src/sections/CategoryCardsSection/index.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.scss'
 import CategoryCards from '../../components/CategoryCards'
 import { category_cards } from '../../data/category-cards'
 
 export default function CategoryCardsSection() {
+  const [activeIndex, setActiveIndex] = useState(0)
+
   return (
     <section className='flex_center category_cards' >
       <div className='flexrow_center_between category_cards_container'>
         {category_cards.map((item, index) => (
-          <CategoryCards data={item} key={index}/>
+          <CategoryCards data={item} key={index} active={index === activeIndex} onClick={() => setActiveIndex(index)}/>
         ))}
       </div>
     </section>
