d4cefe1c62693b346cfb4814396f58a95b43b432
Hiago <hiago@redrex.tech>
2026-04-03 11:09:31 -0300

    Seção newsletter e fotter

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,6 +5,8 @@ import MainBanner from '../../sections/MainBanner'
 import CategoryCardsSection from '../../sections/CategoryCardsSection'
 import Partners from '../../sections/Partners'
 import Brands from '../../sections/Brands'
+import Newsletter from '../../sections/Newsletter'
+import Footer from '../../sections/Footer'
 
 export default function Home() {
   return (
@@ -14,6 +16,8 @@ export default function Home() {
       <CategoryCardsSection />
       <Partners />
       <Brands />
+      <Newsletter />
+      <Footer />
     </main>
   )
 }
---
+++ src/sections/Newsletter/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './style.scss'
+
+export default function Newsletter() {
+  return (
+    <section className='flex_center newssletter'>
+      <div className='w_1280 flexrow_center_between newssletter_container'>
+        <div className='newssletter_left_content'>
+          <h3>Inscreva-se na nossa newsletter
+            <p>Assine a nossa newsletter e receba as novidades e conteúdos exclusivos da Econverse.</p>
+          </h3>
+        </div>
+        <div className='newssletter_right_content'>
+          <form >
+            <input type="text" placeholder='Digite seu nome' />
+            <input type="text" placeholder='Digite seu e-mail' />
+            <input type="submit" value="INSCREVER" />
+          </form>
+          <div className='checkbox'>
+            <input type="checkbox" />
+            <span>Aceito os termos e condições</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
